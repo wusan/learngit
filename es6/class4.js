e398b3611974432cee066209027df439d03da916
ycdeng <ycdeng@isoftstome.com>
2017-03-21 15:34:30 +0800

    class4-5.js

---
+++ es6/class4.js
@@ -0,0 +1,99 @@
+﻿/**
+	super关键字：即可当函数用也可当对象用
+	1.当函数调用：代表父类的构造函数.ES6要求，子类的构造函数必须执行一次super函数
+	       super()只能用在子类的构造函数中，用在其他地方报错
+				m(){
+					super(); // 报错
+				}		
+	2.作为对象：指向父类的原型对象prototype,故无法访问构造函数上的属性或方法	
+	3.ES6规定:通过super调用父类的方法时，super会绑定子类的this
+
+
+	
+**/
+	
+class A{
+	constructor(){
+		this.a="super访问不到";
+		this.xyz="父类---999";
+	}
+	p(){
+		console.log("父类的p方法");
+	}
+	print() {
+		console.log("66:",this.xyz);
+	}
+}
+class B extends A{
+	constructor(){
+		super();//代表调用父类的构造函数。必须，否则报错
+		/**
+			super()相当于：A.prototype.constructor.call(this)
+			this是子类的
+		**/
+		super.p();//当对象使用：
+		
+		/**
+			super为A.prototype    super.p() 相当于A.prototype.p()
+		**/
+		
+		this.xyz="子类——————888";
+		
+	}
+	get m2(){
+		return super.a;//访问不到的。。
+	}
+	getX(){
+		super.print();//调用父类的print方法，但this是子类的 。
+		this.print();//调用子类的print方法。
+	}
+	print() {
+		console.log("77:",this.xyz);
+	}
+}
+let a2=new B();
+console.log(a2.a);//可以访问
+//console.log(a2.m2());//报错。。  get的用法。。。。
+a2.getX();/**
+	66: 子类——————888 
+	77: 子类——————888
+**/
+
+
+class A2 {
+  constructor() {
+    this.x = 1;
+  }
+  c6(){
+	  console.log(this.x)
+  }
+}
+
+class B2 extends A2 {
+  constructor() {
+    super();
+    this.x = 2222;
+    super.x = 300;//无效。。。。
+  }
+  a3(){
+	console.log("super.x:",super.x,A2.x); // undefined  undefined
+    console.log("this.x:",this.x); // 2222  
+  }
+}
+
+let b2 = new B2();
+b2.a3()
+let a8 = new A2();
+a8.c6();//1
+
+/**
+实例的__proto__属性
+	1.子类的原型的原型，是父类的原型，即子类实例的__proto__的__proto 等于父类实例的 __proto__
+	2.通过子类实例的__proto__.__proto__属性，可以修改父类实例的行为
+**/
+console.log("子类的原型的原型，是父类的原型",b2.__proto__.__proto__===a8.__proto__);//true
+b2.__proto__.__proto__.printName = function () {
+  console.log('巴拉巴拉');
+};//子类实例的__proto__.__proto__属性，修改父类实例的method
+a8.printName();//巴拉巴拉
+
---
+++ es6/class5.js
@@ -0,0 +1,89 @@
+﻿/**
+	Class的取值函数(getter)、存值函数(setter)
+**/
+class MyClass{
+	constructor(){
+		
+	}
+	get p2(){
+		console.log('getter p2时自动调用');
+	}
+	set p2(val){
+		console.log('setter p2时自动调用',val);
+	}
+}
+let inst=new MyClass();
+inst.p2=300;//自动执行：set p2(val)
+inst.p2;//自动执行： get p2()
+
+/**
+静态方法static
+	1.static关键字，表示方法不被实例继承，而是直接通过类来调用
+	2.父类的静态方法，可以被子类继承
+	3.Class内部只有静态方法，没有静态属性
+**/
+class Foo{
+	static p2(){
+		console.log("hello");
+	}
+}
+Foo.p2();//hello               通过类调用而不是实例
+var f2=new Foo();
+//f2.p2();					 //报错,不能 通过实例调用
+
+class Bar extends Foo{
+	
+}
+Bar.p2();//hello
+Bar.a2=300;//这样实现静态属性
+/**
+new.target属性
+	1.new.target属性:（构造函数）返回new命令作用于的那个构造函数。
+					如果构造函数不是通过new命令调用的，new.target会返回undefined，
+
+	2.Class内部调用new.target，返回当前Class
+	3.子类继承父类时，new.target会返回子类	
+**/
+function Person(name){
+	if(new.target!==undefined){
+		this.name=name;
+	}else{
+		throw new  Error("必须使用new生成实例")
+	}
+}
+var per=new Person('a3');
+//Person('a3');//报错。。。。。
+
+
+class Rectangle {
+  constructor(length, width) {
+    console.log("Class内部调用new.target，返回当前Class:",new.target === Rectangle, " new.target值：",new.target===Square);//false false
+  }
+}
+
+
+class Square extends Rectangle {
+  constructor(length) {
+    super(length, length);
+	console.log("kk:",new.target===Square);//true
+  }
+}
+
+var obj = new Rectangle(3, 4); // 输出 true
+var obj = new Square(3);// 输出 false
+//利用这个特点，可以写出不能独立使用、必须继承后才能使用的类
+
+class PersonC{
+	constructor(){
+		if(new.target===PersonC){
+			throw new  Error("本类不能实例化");
+		}
+	}
+}
+class Man extends PersonC{
+	constructor(){
+		super();
+	}
+}
+var y= new Man();//正确
+//var x= new PersonC();//错误
